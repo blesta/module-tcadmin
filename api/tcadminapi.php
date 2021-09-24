b92dfcb13f8a52499c16fc680b89e0f7be99dc6c
Jonathan Reissmueller <jonathan@phillipsdata.com>
2021-09-24 09:52:23 -0700

    Updated Verify Host on cURL call

--- api/tcadminapi.php
+++ api/tcadminapi.php
@@ -71,7 +71,7 @@ class Tcadminapi
 
         if (Configure::get('Blesta.curl_verify_ssl')) {
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, true);
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
         } else {
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
